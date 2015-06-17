62b83accf33bb408e1a05f9c372d91693fa1d22f
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-17 17:28:04 +0100

    Breaking out static content

--- server.js
+++ server.js
@@ -7,11 +7,9 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
-app.use('/api/posts', require('./app/controllers/api/posts'))
 
-app.get('/', function(req, res) {
-  res.sendFile('app/layouts/posts.html', { root: __dirname });
-});
+app.use('/', require('./app/controllers/static'))
+app.use('/api/posts', require('./app/controllers/api/posts'))
 
 app.listen(3000, function() {
   console.log('Server listening on', 3000);
