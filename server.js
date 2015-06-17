9aa24282e65dda1a6431003c3bb91ade4e097e12
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-17 16:04:24 +0100

    Broken out API code

--- server.js
+++ server.js
@@ -7,30 +7,10 @@ var app = express();
 
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
+app.use('/api/posts', require('./app/controllers/api/posts'))
 
 app.get('/', function(req, res) {
   res.sendFile('app/layouts/posts.html', { root: __dirname });
-});
-
-app.get('/api/posts', function(req, res, next) {
-  Post.find()
-    .sort('-date')
-    .exec(function (err, posts) {
-      if (err) { return next(err) }
-      res.json(posts);
-  });
-});
-
-app.post('/api/posts', function(req, res, next) {
-  var post = new Post({
-    username: req.body.username,
-    body: req.body.body
-  });
-
-  post.save(function(err, post) {
-    if (err) { return next(err) }
-    res.status(201).json(post);
-  });
 });
 
 app.listen(3000, function() {
