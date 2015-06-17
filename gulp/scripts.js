78a47b722804cd752fe3f71c0378932967a87c59
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-17 22:59:03 +0100

    Added Gulp automation

---
+++ gulp/scripts.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
+
+gulp.task('js', function () {
+  gulp.src(['app/ng/module.js', 'app/ng/**/*.js'])
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.js'))
+      .pipe(ngAnnotate())
+      .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('app/assets'));
+});
+
+gulp.task('watch:js', ['js'], function () {
+  gulp.watch('app/ng/**/*.js', ['js']);
+});
