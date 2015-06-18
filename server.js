589b29cbc2c98068d0b16266219ce17622b0e44a
Augustinas Markevicius <augustinas.markevicius@gmail.com>
2015-06-18 20:24:36 +0100

    Set up protractor

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
+var port = process.env.PORT || 3000
 var express = require('express');
 var bodyParser = require('body-parser');
-
-var Post = require('./app/models/post');
 
 var app = express();
 
@@ -17,6 +16,6 @@ app.use('/api/posts', require('./app/controllers/api/posts'));
 app.use('/api/sessions', require('./app/controllers/api/sessions'));
 app.use('/api/users', require('./app/controllers/api/users'));
 
-app.listen(3000, function() {
-  console.log('Server listening on', 3000);
+var server = app.listen(port, function() {
+  console.log('Server', process.pid, 'listening on', port);
 });
